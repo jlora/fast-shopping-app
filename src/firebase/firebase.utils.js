162670fd1140282ba6d78f2f9ccd9c293cbcb85a
System Administrator <root@iMac-de-Andres.local>
2020-06-23 19:11:12 -0600

    setting up e-commerce project, added React Router and Routing, implements forms and components, Firabase authentication and colletions storage to test app, Redux in Our App, Session & Local Storage and Redux Persist, Stripe Payments, Deploying to Heroku, CSS in JS Styled Components, Redux Thunk, Container Pattern, Cart Item and Checkout page

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,83 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyCpTeqWXWWvq-9y8SNlL1v6HFd6P8iK88s",
+  authDomain: "crwn-db-c001e.firebaseapp.com",
+  databaseURL: "https://crwn-db-c001e.firebaseio.com",
+  projectId: "crwn-db-c001e",
+  storageBucket: "crwn-db-c001e.appspot.com",
+  messagingSenderId: "1054201950783",
+  appId: "1:1054201950783:web:618eccb95a8c99910abe8a",
+  measurementId: "G-F0CZKTQ2VE"
+};
+
+firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
